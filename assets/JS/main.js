0d6b35b22a963a0176acb1c81ea95a0d8845aae9
Ed Carlos Fernandes <92828340+EdCarlos-Fernandes@users.noreply.github.com>
2023-09-08 11:40:40 -0300

    Delete assets directory

--- assets/JS/main.js
+++
@@ -1,5 +0,0 @@
-let menuHamburguer = document.querySelector('#iconNavbarSidenav');
-menuHamburguer.addEventListener('click', () => {
-    let sideNav = document.querySelector('#sidenav-main');
-    sideNav.style.transform = 'translateX(0)';
-});
